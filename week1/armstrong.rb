287891a85dd9a72c012cf23c73df915b7660be08
ayush4345 <ayushk@LAPTOP-PTFJQL0B.localdomain>
2022-02-11 16:32:23 +0530

    done with assignment

--- week1/armstrong.rb
+++ week1/armstrong.rb
@@ -1,13 +1,21 @@
-# An n-digit number that is the sum of the nth powers of its digits is called an n-narcissistic number. It is also known as an Armstrong number.
-#
-# For example,
-# 153 = 1^3 + 5^3 + 3^3
-#
-# `n` is a number provided to you as input.
-#
-# Your output should be `true` if the number is an Armstrong number
-# and `false` otherwise.
+
 
 def armstrong_number(n)
-    raise NotImplementedError # Delete this line and start coding here
+    if n<=0
+        return false
+    else
+        number = n
+        power = n.to_s.length
+        sum=0
+        for i in 1..n do
+            sum=(number%10)**power
+            number=number/10
+        if sum==n
+            return true
+        else
+            return false
+        end
+    end    
+
+
 end
